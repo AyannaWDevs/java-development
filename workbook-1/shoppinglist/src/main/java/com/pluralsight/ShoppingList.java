e529454e321ce7a4fe26b1d75529e8598a596ea8
Ayanna Williams <ayanna.williams20@att.net>
2024-09-26 10:48:47 -0400

    completed ShoppingList app

---
+++ workbook-1/shoppinglist/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String [] groceryItems = {"steak", "milk", "jelly beans", "apples", "bananas", "bread", "cheese", "butter", "eggs", "bacon"};
+        for (String i : groceryItems)
+        {
+            System.out.println(i);
+        }
+    }
+}
